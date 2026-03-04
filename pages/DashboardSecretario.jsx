7a41b310fc863eff16fb0b15031ce563d3b38962
authsecurit21-web <authsecurit21@gmail.com>
2026-03-03 22:42:46 -0300

    Create DashboardSecretario.jsx

---
+++ pages/DashboardSecretario.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DashboardSecretario() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Painel do Secretário</h1>
+      <p>Secretário pode ver tudo e postar eventos e passeios escolares.</p>
+    </div>
+  );
+}
