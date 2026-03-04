92fe0cb115f058eeace1ec4a8656b38531d79e1e
authsecurit21-web <authsecurit21@gmail.com>
2026-03-03 22:42:11 -0300

    Create DashboardCoordenador.jsx

---
+++ pages/DashboardCoordenador.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DashboardCoordenador() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Painel do Coordenador</h1>
+      <p>Coordenador pode alterar atividades, eventos e passeios escolares.</p>
+    </div>
+  );
+}
