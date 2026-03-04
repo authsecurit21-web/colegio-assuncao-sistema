0df9572b0a517c9e5006f615add23777f5a92813
authsecurit21-web <authsecurit21@gmail.com>
2026-03-03 22:41:41 -0300

    Create DashboardProfessor.jsx

---
+++ pages/DashboardProfessor.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DashboardProfessor() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Painel do Professor</h1>
+      <p>O professor pode postar atividades, editar boletins e agenda.</p>
+    </div>
+  );
+}
