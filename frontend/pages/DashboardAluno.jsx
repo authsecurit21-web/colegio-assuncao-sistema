9b9abfec100f2f1dac94aa92e993d118a9c52e9c
authsecurit21-web <authsecurit21@gmail.com>
2026-03-03 22:50:37 -0300

    Create DashboardAluno.jsx

---
+++ frontend/pages/DashboardAluno.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DashboardAluno() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Painel do Aluno</h1>
+      <p>O aluno pode ver boletim, agenda e ocorrências.</p>
+    </div>
+  );
+}
